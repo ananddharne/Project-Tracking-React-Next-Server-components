87880dec82b25343db3f6dce90182f97ff04649f
Anand Dharne <anand.dharne24@gmail.com>
2023-02-10 15:54:34 -0500

    add api for create project and task and project components

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -5,6 +5,8 @@ import { cookies } from "next/headers";
 import Greetings from "@/components/Greetings"
 import GreetingsSkeleton from "@/components/GreetingsSkeleton"
 import ProjectCard from "@/components/ProjectCard"
+import TasksCard from "@/components/TasksCard"
+import NewProject from "@/components/NewProject"
 
 import Link from "next/link";
 import { Suspense } from "react";
@@ -12,6 +14,7 @@ import { Suspense } from "react";
 export default async function Page() {
 
   const getData = async () => {
+    delay(2000)
     const user = await getUserFromCookie(cookies());
     const projects = await db.project.findMany({
       where: {
@@ -26,7 +29,7 @@ export default async function Page() {
   };
   const { projects } = await getData();
   return (
-    <div className="h-full overflow-y-auto pr-6 w-full">
+    <div className="h-full overflow-y-auto px-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
         <Suspense fallback={<GreetingsSkeleton />}>
@@ -44,10 +47,15 @@ export default async function Page() {
                 </div>
               ))
           }
-          <div className="w-1/3 p-3">{/* new project here */}</div>
+          <div className="w-1/3 p-3">
+          <NewProject />
+          </div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
-          <div className="w-full">{/* tasks here */}</div>
+          <div className="w-full">
+          <TasksCard />
+          
+          </div>
         </div>
       </div>
     </div>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-// import { hashPassword } from "@/lib/auth";
+import { hashPassword } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
 
@@ -15,18 +15,12 @@ async function main() {
   const user = await db.user.upsert({
     where: { email: "dharneanand92@gmail.com" },
     update: {
-      projects: {
-        create: new Array(5).fill(1).map((_, i) => ({
-          name: `Project ${i}`,
-          due: new Date(2022, 11, 25),
-        })),
-      },
     },
     create: {
       email: "user@email.com",
       firstName: "User",
       lastName: "Person",
-      password: 'password',
+      password: await hashPassword("password"),
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
