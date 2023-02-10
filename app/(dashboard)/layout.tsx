0fc7f2e2f4baffc2f542f267414b2ddadeaa7bcb
Anand Dharne <anand.dharne24@gmail.com>
2023-02-10 14:27:48 -0500

    set up project with prisma db, project and dashboard layout - rough

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '@/styles/global.css'
+import GlassPane from "../../components/GlassPane";
+import Sidebar  from "../../components/SideBar";
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center">
+          <Sidebar />
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
